8193255986479cae50227d9d6ff7bf19a1708903
koteswaradk <koteswara.d.k@gmail.com>
2026-09-16 12:41:15 +0530

    version code updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.akshaglobal.smartcallshield"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 7
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
